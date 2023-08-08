d681465a4904f610b9ae7f2535a370431f1590b2
sypark2 <sypark2@businessinsight.co.kr>
2023-08-09 07:43:06 +0900

    Initial commit

---
+++ src/test/java/com/example/th1/Th1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.th1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Th1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
